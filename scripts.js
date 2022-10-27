17da8eea0d4fe1b845e8cbaaaae1d3413e83bd89
Shinchoku <tienmp3@gmail.com>
2022-10-27 10:04:16 +0700

    #add position

--- scripts.js
+++ scripts.js
@@ -4,14 +4,26 @@ $(function () {
   var mapLat = 21.006423;
   var mapLng = 105.841394;
   var mapDefaultZoom = 12;
-  initialize_map();
-  function initialize_map() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        mapLat = pos.coords.latitude;
+        mapLng = pos.coords.longitude;
+        initialize_map(true);
+      },
+      () => {
+        initialize_map(false);
+      }
+    );
+  }
+
+  function initialize_map(isPos) {
     layerBG = new ol.layer.Tile({
-      className : 'osm-layer',
+      className: "osm-layer",
       source: new ol.source.OSM({}),
     });
     var layer_bg = new ol.layer.Image({
-      className : 'bg-layer',
+      className: "bg-layer",
       source: new ol.source.ImageWMS({
         ratio: 1,
         url: "http://localhost:8080/geoserver/internetcafe/wms",
@@ -24,7 +36,7 @@ $(function () {
       }),
     });
     var layer_ic = new ol.layer.Image({
-      className : 'point-layer',
+      className: "point-layer",
       source: new ol.source.ImageWMS({
         ratio: 1,
         url: "http://localhost:8080/geoserver/internetcafe/wms",
@@ -42,16 +54,62 @@ $(function () {
     });
     map = new ol.Map({
       target: "map",
-      layers: [layerBG,layer_bg,layer_ic],
+      layers: [layerBG, layer_bg, layer_ic],
       view: viewMap,
     });
+
+    if (isPos) {
+      const geolocation = new ol.Geolocation({
+        tracking: true,
+        trackingOptions: {
+          enableHighAccuracy: true,
+        },
+        projection: viewMap.getProjection(),
+      });
+      geolocation.on("error", function (error) {
+        alert("Vui lòng gps");
+      });
+      const accuracyFeature = new ol.Feature({
+        geometry:geolocation.getAccuracyGeometry()
+      });
+      geolocation.on("change:accuracyGeometry", function () {
+        accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+      });
+
+      const positionFeature = new ol.Feature();
+      positionFeature.setStyle(
+        new ol.style.Style({
+          image: new ol.style.Circle({
+            radius: 5,
+            fill: new ol.style.Fill({
+              color: "#3399CC",
+            }),
+            stroke: new ol.style.Stroke({
+              color: "#fff",
+              width: 2,
+            }),
+          }),
+        })
+      );
+
+      geolocation.on("change:position", function () {
+        const coordinates = geolocation.getPosition();
+        positionFeature.setGeometry(
+          coordinates ? new ol.geom.Point(coordinates) : null
+        );
+      });
+      new ol.layer.Vector({
+        map: map,
+        source: new ol.source.Vector({
+          features: [accuracyFeature, positionFeature],
+        }),
+      });
+    }
     map.render();
   }
 
-
-/* When the user clicks on the button,
+  /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-
 });
 
 function filterFunction() {
